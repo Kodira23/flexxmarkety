431420506bb5a8858aae834525cf935e52223c98
Kodira23 <Odirasteve25@gmail.com>
2026-04-11 01:59:49 -0700

    Update Ticker.jsx

--- src/components/Ticker.jsx
+++ src/components/Ticker.jsx
@@ -2,27 +2,26 @@ import { useTicker } from '../hooks/useTicker'
 import './Ticker.css'
 
 const COIN_LOGOS = {
-  BTC:  'https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png',
-  ETH:  'https://assets.coingecko.com/coins/images/279/thumb/ethereum.png',
-  BNB:  'https://assets.coingecko.com/coins/images/825/thumb/bnb-icon2_2x.png',
-  SOL:  'https://assets.coingecko.com/coins/images/4128/thumb/solana.png',
-  XRP:  'https://assets.coingecko.com/coins/images/44/thumb/xrp-symbol-white-128.png',
-  ADA:  'https://assets.coingecko.com/coins/images/975/thumb/cardano.png',
-  DOGE: 'https://assets.coingecko.com/coins/images/5/thumb/dogecoin.png',
-  TRX:  'https://assets.coingecko.com/coins/images/1094/thumb/tron-logo.png',
-  AVAX: 'https://assets.coingecko.com/coins/images/12559/thumb/Avalanche_Circle_RedWhite_Trans.png',
-  LINK: 'https://assets.coingecko.com/coins/images/877/thumb/chainlink-new-logo.png',
-  DOT:  'https://assets.coingecko.com/coins/images/12171/thumb/polkadot.png',
-  MATIC:'https://assets.coingecko.com/coins/images/4713/thumb/matic-token-icon.png',
-  LTC:  'https://assets.coingecko.com/coins/images/2/thumb/litecoin.png',
-  UNI:  'https://assets.coingecko.com/coins/images/12504/thumb/uniswap-uni.png',
-  ATOM: 'https://assets.coingecko.com/coins/images/1481/thumb/cosmos_hub.png',
+  BTC:   'https://assets.coingecko.com/coins/images/1/large/bitcoin.png',
+  ETH:   'https://assets.coingecko.com/coins/images/279/large/ethereum.png',
+  BNB:   'https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png',
+  SOL:   'https://assets.coingecko.com/coins/images/4128/large/solana.png',
+  XRP:   'https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png',
+  ADA:   'https://assets.coingecko.com/coins/images/975/large/cardano.png',
+  DOGE:  'https://assets.coingecko.com/coins/images/5/large/dogecoin.png',
+  TRX:   'https://assets.coingecko.com/coins/images/1094/large/tron-logo.png',
+  AVAX:  'https://assets.coingecko.com/coins/images/12559/large/Avalanche_Circle_RedWhite_Trans.png',
+  LINK:  'https://assets.coingecko.com/coins/images/877/large/chainlink-new-logo.png',
+  DOT:   'https://assets.coingecko.com/coins/images/12171/large/polkadot.png',
+  MATIC: 'https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png',
+  LTC:   'https://assets.coingecko.com/coins/images/2/large/litecoin.png',
+  UNI:   'https://assets.coingecko.com/coins/images/12504/large/uniswap-uni.png',
+  ATOM:  'https://assets.coingecko.com/coins/images/1481/large/cosmos_hub.png',
 }
 
 export default function Ticker() {
   const pairs = useTicker()
   const doubled = [...pairs, ...pairs]
-
   return (
     <div className="ticker-wrapper">
       <div className="ticker-track">
@@ -30,7 +29,6 @@ export default function Ticker() {
           const base = p.symbol.split('/')[0]
           const logo = COIN_LOGOS[base]
           const isUp = p.change >= 0
-
           return (
             <span key={i} className="ticker-item">
               {logo && (
@@ -46,7 +44,7 @@ export default function Ticker() {
                 {p.price.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 4 })}
               </span>
               <span className={`ticker-change ${isUp ? 'up' : 'down'}`}>
-                {isUp ? '+' : ''}{p.change.toFixed(2)} ({isUp ? '+' : ''}{p.change.toFixed(2)}%)
+                {isUp ? '+' : ''}{p.change.toFixed(2)}%
               </span>
             </span>
           )
